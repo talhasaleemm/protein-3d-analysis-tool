2f584f9354517c301d495f30b58f9edcbb410292
Muhammad Talha saleem <75055604+talhasaleemm@users.noreply.github.com>
2025-03-24 15:13:56 +0500

    Update plot_utils.py

---
+++ src/visualization/plot_utils.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+
+def fig_to_base64(fig):
+    buf = BytesIO()
+    fig.savefig(buf, format='png', bbox_inches='tight')
+    buf.seek(0)
+    img_str = base64.b64encode(buf.read()).decode('utf-8')
+    plt.close(fig)
+    return img_str
